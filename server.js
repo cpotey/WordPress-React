0355d2a22356d00faa0be549ac4b163a9c04e1c3
Connor Pote <cpotey@me.com>
2019-05-24 12:18:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app
       return handle(req, res)
     })
 
-    server.listen(3000 || process.env.PORT, err => {
+    server.listen(process.env.PORT || 3000, err => {
       if (err) throw err
       console.log('> Ready on http://localhost:3000')
     })
